b88b425ab959f6a5547432d9d32c4ea05cae7582
Klassichacker <162910617+Klassichacker@users.noreply.github.com>
2024-03-10 19:44:43 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['2348089721406', 'Gifted Minds Tutor bot', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
